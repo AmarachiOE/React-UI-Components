69ffe708707c97eef5d0079988e1662afe07ad13
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-18 15:23:10 -0700

    added rest of MVP css styling to match design-file

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <div className="app">
       <HeaderContainer />
       <CardContainer />
-      <h3>Welcome to React Social Card!</h3>
+      {/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -25,7 +25,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
