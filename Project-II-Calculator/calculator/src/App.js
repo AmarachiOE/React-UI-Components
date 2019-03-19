52e777173184686fac073654fdd1d6fa320eec97
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-19 13:40:19 -0700

    updated NumberButton.js Create NumberButton child component that receives props and returns a button with specific info declared in the parent component App in  App.js. Used CSS grid to style the buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
 
+// import custom components
+import NumberButton from "./components/ButtonComponents/NumberButton";
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app grid-container">
+      <NumberButton gridItem="item3" buttonType="symbol" text="/" />
+      <NumberButton gridItem="item4" buttonType="number" text="7" />
+      <NumberButton gridItem="item5" buttonType="number" text="8" />
+      <NumberButton gridItem="item6" buttonType="number" text="9" />
+      <NumberButton gridItem="item7" buttonType="symbol" text="x" />
+      <NumberButton gridItem="item8" buttonType="number" text="4" />
+      <NumberButton gridItem="item9" buttonType="number" text="5" />
+      <NumberButton gridItem="item10" buttonType="number" text="6" />
+      <NumberButton gridItem="item11" buttonType="symbol" text="-" />
+      <NumberButton gridItem="item12" buttonType="number" text="1" />
+      <NumberButton gridItem="item13" buttonType="number" text="2" />
+      <NumberButton gridItem="item14" buttonType="number" text="3" />
+      <NumberButton gridItem="item15" buttonType="symbol" text="+" />
+      <NumberButton gridItem="item16" buttonType="number" text="0" />
+      <NumberButton gridItem="item17" buttonType="symbol" text="=" />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return <button className={`basicButtonStyles ${props.gridItem} ${props.buttonType}`}> {props.text} </button>;
+};
+
+// declare defaults... is this doing anything???
+NumberButton.defaultProps = {
+    buttonColor: 'white',
+    // buttonColor: 'pink', works
+};
+
+export default NumberButton;
+
+// import custom component on app.js file
